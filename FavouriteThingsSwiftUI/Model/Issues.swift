83cd2f01d74c29c6e02918ded91589bafab2f066
Chen Sihan <schen@lcs.on.ca>
2021-02-01 16:11:44 -0500

    Add more questions to the FAQ view Created a home view

--- FavouriteThingsSwiftUI/Model/Issues.swift
+++ FavouriteThingsSwiftUI/Model/Issues.swift
@@ -30,7 +30,21 @@ struct Issues {
         
         list.append(Issue(question: "Can day students sleep over on campus?", answerText: "Day students are welcome to sleep on campus with their boarding student friends on Saturday evenings or on Fridays of Quiet Weekends. In exceptional circumstances (like early morning athletic fixtures) day students may be permitted to sleep on campus. If a day student wishes to spend an overnight on campus, they are to contact their day Head of House two days prior to the overnight.", systemImageName: systemName, detailViewTitle: "Sleep Over"))
         
-        list.append(<#T##newElement: Issue##Issue#>)
+        list.append(Issue(question: "When can students return to campus after breaks?", answerText: "Residential staff resume duty at 6:00 p.m. on the day of a return from break. Boarding students should not return to campus until this time. The Dining Hall is not open on the evening of a return from break. Boarding students should be back on campus by 9:00 p.m.", systemImageName: systemName, detailViewTitle: "Return Information"))
+        
+        list.append(Issue(question: "Who do I contact to find out information about student travel for breaks?", answerText: "Student travel information is located in the school’s Quick Reference Guide, which was received by parents at Registration. Further travel information can be requested through the travel form (lcs.on.ca/travelform) or to travelrequest@lcs.on.ca.", systemImageName: systemName, detailViewTitle: "Whom To Contact"))
+        
+        list.append(Issue(question: "Who can I turn to in my time of need?", answerText: "There are many staff and students who are willing and able to help a student get “in The Grove.” Here are some ideas of who to contact if you need support or assistance:\n\n• If you have a question, ask your Head of House or Associate Faculty. If you need medical support, speak to the Health and Well-being Centre staff.\n\n• If you need academic support, contact your Advisor.\n\n• If you need to speak to someone confidentially, reach out to our Health and Well-being Centre staff or another member of our counselling team.\n\n• If you need help with your academic subjects, go to extra-help and talk to your teachers. If you want to learn some tricks for survival at LCS, speak to senior students.\n\n• If you want to figure out how the school operates, ask your Assistant Head: School Life or Head of School.\n\n• If you want to make a friend, be a friend to others.", systemImageName: systemName, detailViewTitle: "Find Support"))
+        
+        list.append(Issue(question: "Why is the school called The Grove?", answerText: "Lakefield College School’s nickname, “The Grove”, derives from the beautiful grove of trees located outside of the Head of School’s residence.\n\nThe Grove is a caring and compassionate community that believes every one of its members can make a positive difference in the world. It is a place of hope, of support, of fun and of pride.", systemImageName: systemName, detailViewTitle: "Grove"))
+        
+        list.append(Issue(question: "Does The Grove have a mascot?", answerText: "Yes. The school has a long history of association with the turtle. The school’s mascot, Grover, is a terrapin. We see many “real” living turtles around our waterfront, and one exceptionally large terrapin roaming about campus on special days and events.", systemImageName: systemName, detailViewTitle: "Grove Mascot"))
+        
+        list.append(Issue(question: "Does the school have a cheer?", answerText: "Yes. “G-R-O-V-E” is chanted and repeated three times, getting louder each time, concluding with a loud “GROVE!” at the end.", systemImageName: systemName, detailViewTitle: "School Cheer"))
+        
+        
+        list.append(Issue(question: "How can I become part of this community as quickly as possible?", answerText: "It may take a while for new students to figure out the school and all of its idiosyncrasies, but once a student gets “in The Grove” it never leaves your system. New students are encouraged to get involved, take risks and extend themselves to try new things and meet new people.", systemImageName: systemName, detailViewTitle: "Tips"))
+        
         
         
     }
---
+++ FavouriteThingsSwiftUI/View/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  SchoolLifeGuideNew
+//
+//  Created by Chen, Sihan on 2021-02-01.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("You will soon realize that being a member of Lakefield College School is to be a part of a close and caring family. Make the most of your time at the school. Before you know it, you will be graduating and bidding farewell to your Grove.")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
