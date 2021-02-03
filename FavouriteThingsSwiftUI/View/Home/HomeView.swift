23dbcedd05a0acfd8f70af3f8ee6230906afe1d7
Chen Sihan <schen@lcs.on.ca>
2021-02-03 18:03:53 -0500

    Add a custom font to the app

--- FavouriteThingsSwiftUI/View/Home/HomeView.swift
+++ FavouriteThingsSwiftUI/View/Home/HomeView.swift
@@ -16,10 +16,11 @@ struct HomeView: View {
                     BadgeBackground().scaledToFit()
                     VStack {
                         Text("Title")
-                            .padding(.top,150)
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .padding(.top, 137)
+                            .font(Font.custom("Comic Sans MS", size: 33))
                         Spacer()
-                        Text("You will soon realize that being a member of Lakefield College School is to be a part of a close and caring family. Make the most of your time at the school. Before you know it, you will be graduating and bidding farewell to your Grove.").padding(.all,47)
+                        Text("You will soon realize that being a member of Lakefield College School is to be a part of a close and caring family. Make the most of your time at the school. Before you know it, you will be graduating and bidding farewell to your Grove.").padding(.bottom,140)
+                            .padding(.horizontal, 47)
                     }
                     
                 }
