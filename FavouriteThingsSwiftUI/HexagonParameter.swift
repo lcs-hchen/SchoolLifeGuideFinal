367506602c967f4dd6d8ffce5ca965bd8a4a58a8
Chen Sihan <schen@lcs.on.ca>
2021-02-03 17:38:04 -0500

    add a system built background hexagon for home page

--- FavouriteThingsSwiftUI/HexagonParameter.swift
+++
@@ -1,8 +0,0 @@
-//
-//  HexagonParameter.swift
-//  SchoolLifeGuideNew
-//
-//  Created by Chen, Sihan on 2021-02-03.
-//
-
-import Foundation
--- FavouriteThingsSwiftUI/View/Home/HomeView.swift
+++ FavouriteThingsSwiftUI/View/Home/HomeView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("You will soon realize that being a member of Lakefield College School is to be a part of a close and caring family. Make the most of your time at the school. Before you know it, you will be graduating and bidding farewell to your Grove.").padding(.horizontal)
+        
+        ScrollView{
+            VStack {
+                ZStack {
+                    BadgeBackground().scaledToFit()
+                    VStack {
+                        Text("Title")
+                            .padding(.top,150)
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        Spacer()
+                        Text("You will soon realize that being a member of Lakefield College School is to be a part of a close and caring family. Make the most of your time at the school. Before you know it, you will be graduating and bidding farewell to your Grove.").padding(.all,47)
+                    }
+                    
+                }
+                
+                BadgeBackground()
+            }
+        }
+       
+        
     }
 }
 
