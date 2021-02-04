57ae561e4307344f18bebed9f1d41363bb2db01f
Chen Sihan <schen@lcs.on.ca>
2021-02-04 00:16:50 -0500

    Adjusting the views for school values

--- FavouriteThingsSwiftUI/View/Home/HomeView.swift
+++ FavouriteThingsSwiftUI/View/Home/HomeView.swift
@@ -16,128 +16,28 @@ struct HomeView: View {
                 
                 
                 
-                MessageFunction(firstLineTitle: "Trust", description: "A shared belief in the moral strength, character and reliability of each member of our community that results in feelings of mutual confidence and mutual respect.", titleFontSize: 33, textFontSize: 15, firstTitleTopPadding: 130, descriptionBottomPadding: 147, descriptionHorizontalPadding: 47)
+                MessageFunction(firstLineTitle: "Trust", description: "A shared belief in the moral strength, character and reliability of each member of our community that results in feelings of mutual confidence and mutual respect.", titleFontSize: 33, textFontSize: 15, firstTitleTopPadding: 130, descriptionBottomPadding: 147, descriptionHorizontalPadding: 77)
                 
                 
                 
+                MessageFunction(firstLineTitle: "Individuality", description: "By celebrating the unique gifts and qualities brought by the individual to the community, we encourage the development of personal values in the context of responsibility to the community and to oneself.", titleFontSize: 29.5, textFontSize: 15, firstTitleTopPadding: 120, descriptionBottomPadding: 130, descriptionHorizontalPadding: 77)
                 
-//                ZStack {
-//                    BadgeBackground().scaledToFit()
-//
-//                    VStack {
-//                        Text("Trust")
-//                            .padding(.top, 130)
-//                            .font(Font.custom("Comic Sans MS", size: 33))
-//                            .foregroundColor(.white)
-//                        Spacer()
-//                        Text("A shared belief in the moral strength, character and reliability of each member of our community that results in feelings of mutual confidence and mutual respect.").padding(.bottom,147)
-//                            .padding(.horizontal, 47)
-//                            .font(Font.custom("Comic Sans MS", size: 15))
-//                            .foregroundColor(.white)
-//                    }
-//
-//                }
-               
+                MessageFunction(firstLineTitle: "Learning", description: "By engaging in a variety of meaningful learning experiences, LCS encourages curiosity, promotes collaborative problem solving, and instills an enduring passion for life-long learning.", titleFontSize: 30, textFontSize: 15, firstTitleTopPadding: 120, descriptionBottomPadding: 147, descriptionHorizontalPadding: 77)
                 
+                MessageFunction(firstLineTitle: "Citizenship", description: "Through service to others and the development of leadership skills we encourage our students to become thoughtful, constructive and contributing members of the local and global community.", titleFontSize: 30, textFontSize: 15, firstTitleTopPadding: 125, descriptionBottomPadding: 147, descriptionHorizontalPadding: 77)
                 
-                MessageFunction(firstLineTitle: "Trust", description: "A shared belief in the moral strength, character and reliability of each member of our community that results in feelings of mutual confidence and mutual respect.", titleFontSize: 33, textFontSize: 15, firstTitleTopPadding: 130, descriptionBottomPadding: 147, descriptionHorizontalPadding: 47)
+                MessageFunction(firstLineTitle: "Environmental", secondLineTitle: "Stewardship", description: "Through our unique campus and programs, our students are inspired to become leaders with a commitment to global environmental stewardship.", titleFontSize: 30, textFontSize: 15, firstTitleTopPadding: 125, descriptionBottomPadding: 130, descriptionHorizontalPadding: 77)
                 
-                ZStack {
-                    BadgeBackground().scaledToFit()
-                    VStack {
-                        Text("Individuality")
-                            .padding(.top, 130)
-                            .font(Font.custom("Comic Sans MS", size: 33))
-                            .foregroundColor(.white)
-                        Spacer()
-                        Text("By celebrating the unique gifts and qualities brought by the individual to the community, we encourage the development of personal values in the context of responsibility to the community and to oneself.").padding(.bottom,147)
-                            .padding(.horizontal, 47)
-                            .font(Font.custom("Comic Sans MS", size: 15))
-                            .foregroundColor(.white)
-                    }
-                    
-                }
-                ZStack {
-                    BadgeBackground().scaledToFit()
-                    VStack {
-                        Text("Learning")
-                            .padding(.top, 130)
-                            .font(Font.custom("Comic Sans MS", size: 33))
-                            .foregroundColor(.white)
-                        Spacer()
-                        Text("By engaging in a variety of meaningful learning experiences, LCS encourages curiosity, promotes collaborative problem solving, and instills an enduring passion for life-long learning.").padding(.bottom,147)
-                            .padding(.horizontal, 47)
-                            .font(Font.custom("Comic Sans MS", size: 15))
-                            .foregroundColor(.white)
-                    }
-                    
-                }
-                ZStack {
-                    BadgeBackground().scaledToFit()
-                    VStack {
-                        Text("Citizenship")
-                            .padding(.top, 130)
-                            .font(Font.custom("Comic Sans MS", size: 33))
-                            .foregroundColor(.white)
-                        Spacer()
-                        Text("Through service to others and the development of leadership skills we encourage our students to become thoughtful, constructive and contributing members of the local and global community.").padding(.bottom,147)
-                            .padding(.horizontal, 47)
-                            .font(Font.custom("Comic Sans MS", size: 15))
-                            .foregroundColor(.white)
-                    }
-                    
-                }
-                ZStack {
-                    BadgeBackground().scaledToFit()
-                    VStack {
-                        Text("Environmental Stewardship")
-                            .padding(.top, 130)
-                            .font(Font.custom("Comic Sans MS", size: 33))
-                            .foregroundColor(.white)
-                        Spacer()
-                        Text("Through our unique campus and programs, our students are inspired to become leaders with a commitment to global environmental stewardship.").padding(.bottom,147)
-                            .padding(.horizontal, 47)
-                            .font(Font.custom("Comic Sans MS", size: 15))
-                            .foregroundColor(.white)
-                    }
-                    
-                }
-                ZStack {
-                    BadgeBackground().scaledToFit()
-                    VStack {
-                        Text("Healthy Caring Community")
-                            .padding(.top, 130)
-                            .font(Font.custom("Comic Sans MS", size: 33))
-                            .foregroundColor(.white)
-                        Spacer()
-                        Text("The belief in the dignity and worth of all individuals. We embrace diversity; we encourage empathy, acceptance and a balanced lifestyle.").padding(.bottom,147)
-                            .padding(.horizontal, 47)
-                            .font(Font.custom("Comic Sans MS", size: 15))
-                            .foregroundColor(.white)
-                    }
-                    
-                }
-                ZStack {
-                    BadgeBackground().scaledToFit()
-                    VStack {
-                        Text("Education of the Whole Person")
-                            .padding(.top, 130)
-                            .font(Font.custom("Comic Sans MS", size: 33))
-                            .foregroundColor(.white)
-                        Spacer()
-                        Text("A commitment to the broad development of the intellectual, social, emotional, spiritual, and physical qualities needed to lead a fulfilling life.").padding(.bottom,147)
-                            .padding(.horizontal, 47)
-                            .font(Font.custom("Comic Sans MS", size: 15))
-                            .foregroundColor(.white)
-                    }
-                    
-                }
+                MessageFunction(firstLineTitle: "Healthy Caring", secondLineTitle: "Community", description: "The belief in the dignity and worth of all individuals. We embrace diversity; we encourage empathy, acceptance and a balanced lifestyle.", titleFontSize: 30, textFontSize: 15, firstTitleTopPadding: 115, descriptionBottomPadding: 135, descriptionHorizontalPadding: 77)
+                
+                MessageFunction(firstLineTitle: "Education of the", secondLineTitle: "Whole Person", description: "A commitment to the broad development of the intellectual, social, emotional, spiritual, and physical qualities needed to lead a fulfilling life.", titleFontSize: 30, textFontSize: 15, firstTitleTopPadding: 125, descriptionBottomPadding: 130, descriptionHorizontalPadding: 77)
+                
                 
                 
             }
             
         }.preferredColorScheme(.dark)
-       
+        
         
     }
 }
--- FavouriteThingsSwiftUI/View/Home/MessageFunction.swift
+++ FavouriteThingsSwiftUI/View/Home/MessageFunction.swift
@@ -30,6 +30,7 @@ struct MessageFunction: View {
                     .foregroundColor(.white)
                 if let secondLine = secondLineTitle {
                     Text(secondLine)
+                        .font(Font.custom("Comic Sans MS", size: titleFontSize))
                 }
                 Spacer()
                 Text(description).padding(.bottom,descriptionBottomPadding)
